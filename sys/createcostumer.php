fe990101feef6219debedb3e088a84c0b820aaaf
isadora walesca alves duarte <83380896+isadorawal@users.noreply.github.com>
2021-10-01 15:24:11 -0300

    Site em php-Parte 1 (até onde consegui)

---
+++ sys/createcostumer.php
@@ -0,0 +1,38 @@
+<section>
+    <h3>
+    <i class="fa fa-users"></i>  <span>Listar Clientes </span>
+</h3>
+</section>
+<section class="content">
+    <div class="box">
+        <div class="box-header with-border">
+          </div>
+              <div class="row margin=vottom-20">
+                <div class="col-md-12 text-right ">
+                    <a href="criar_1.php" title="Novo cadastro" class="btn btn-success"><i class="fa fa-plus-square"></i> Novo </a>
+                </div>
+              </div>
+    </div>
+    
+    
+    <table  class="table table-hover table-sn">
+        <thead>
+            <tr>
+            <th  class="text-center">Nome</th>
+            <th class="text-center">R.G</th>
+            <th  class="text-center">C.P.F</th>
+            <th  class="text-center">Telefone</th>
+            <th  class="text-right">Opções</th>
+           </tr>
+        </thead>
+          <tbody>
+            <tr>
+            <td  class="text-center">Nome</td>
+            <td class="text-center">R.G</td>
+            <td  class="text-center">C.P.F</td>
+            <td  class="text-center">Telefone</td>
+            <td  class="text-right">Opções</td>
+           </tr>
+        </tbody>
+</table>
+</section>
---
+++ sys/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title> Cadastro De Clientes </title>
+    <link rel="stylesheet"  href="public/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+</head>
+    <body>
+<div class="container">
+    <?php include _ _DIR_ _"/views/costumers/list_costumer.php"?>
+</div
+</html>
